bacfdc1fc411421b01cd73aa8dc25d66d01c4bbd
Jaxon Carelos <booga956@gmail.com>
2024-03-19 22:38:42 -0600

    changed how the project is structured

---
+++ EmbedBot/EmbedBot.go
@@ -0,0 +1,37 @@
+package EmbedBot
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"jaxon/embedbot/handler"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func Run() {
+	// get token from .env
+	godotenv.Load()
+	Token := os.Getenv("TOKEN")
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+	dg.AddHandler(handler.MessageCreate)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt)
+	<-sc
+
+	dg.Close()
+}
--- main.go
+++ main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-
-	"jaxon/embedbot/handler"
-
-	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
+	"jaxon/embedbot/EmbedBot"
 )
 
 func main() {
-	// get token from .env
-	godotenv.Load()
-	Token := os.Getenv("TOKEN")
-	dg, err := discordgo.New("Bot " + Token)
-	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
-	}
-	dg.AddHandler(handler.MessageCreate)
-
-	err = dg.Open()
-	if err != nil {
-		fmt.Println("error opening connection,", err)
-		return
-	}
-
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, os.Interrupt)
-	<-sc
-
-	dg.Close()
+	EmbedBot.Run()
 }
