24f3a759b025133ae2f768acfb4c1757e8291978
Jaxon Carelos <booga956@gmail.com>
2024-05-28 11:09:32 -0600

    delete more

--- ContentUtils/ContentUtils_test.go
+++ ContentUtils/ContentUtils_test.go
@@ -5,32 +5,6 @@ import (
 	"testing"
 )
 
-func TestTikTokDownload(t *testing.T) {
-	type args struct {
-		url               string
-		should_be_spoiled bool
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{
-			name: "TikTok Download test 1",
-			args: args{url: "https://www.tiktok.com/t/ZPRKkGh19/", should_be_spoiled: false},
-			want: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			_, _, err := ContentUtils.DownloadTikTokVideo(tt.args.url, tt.args.should_be_spoiled)
-			if err != nil {
-				t.Errorf("DownloadTikTokVideo() error = %v, want %v", err, tt.want)
-			}
-		})
-	}
-}
-
 func TestShouldBeSpoilered(t *testing.T) {
 	type args struct {
 		content string
